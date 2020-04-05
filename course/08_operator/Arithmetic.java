d777c9584a6a9d34c2882293103135e8986c3afb
raynardj <raynard@rasenn.com>
2020-04-05 23:02:48 +0800

    operators

---
+++ course/08_operator/Arithmetic.java
@@ -0,0 +1,22 @@
+public class Arithmetic{
+    public void Arithmetic(){
+    }
+
+    public static void main(String args[]){
+
+        double A = 6;
+        double B = 7;
+        // addition
+        System.out.println(A+B);
+        // subtraction
+        System.out.println(A-B);
+        // multiplication
+        System.out.println(A*B);
+        // division
+        System.out.println(A/B);
+        System.out.println("============");
+        System.out.println(B++);
+        System.out.println(B%A);
+        System.out.println(B--);
+    }
+}
---
+++ course/08_operator/Relational.java
@@ -0,0 +1,12 @@
+public class Relational {
+    public static void main(String args[]){
+        double A = 6;
+        double B = 7;
+
+        System.out.println(A==B);
+        System.out.println(A!=B);
+
+        System.out.println(A>B);
+        System.out.println(A<B);
+    }
+}
