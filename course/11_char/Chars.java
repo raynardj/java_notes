46101e1925f2604002022e3f5958553ba8e07339
raynardj <raynard@rasenn.com>
2020-04-06 23:17:50 +0800

    character

---
+++ course/11_char/Chars.java
@@ -0,0 +1,11 @@
+public class Chars {
+    public static void main(String args[]){
+        // all using single quotes
+        char ch='a';
+        char uniChar = '\u039A';
+        char[] charArray = {'a','b','c','d','e'};
+        System.out.println(ch);
+        System.out.println(uniChar);
+        System.out.println(charArray[2]);
+    }
+}
